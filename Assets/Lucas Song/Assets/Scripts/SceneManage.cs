690adf2087d824e839941bf0e3c2466dbdb20606
Caetano Passos <caetanof2004@gmail.com>
2024-12-03 12:00:00 -0300

    Final Changes

--- Assets/Lucas Song/Assets/Scripts/SceneManage.cs
+++ Assets/Lucas Song/Assets/Scripts/SceneManage.cs
@@ -6,6 +6,6 @@ public class SceneManage : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("Hall");
     }
 }
